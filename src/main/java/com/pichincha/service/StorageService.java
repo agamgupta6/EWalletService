be925efb469673d8b7cfe9be018e3dff7e0cd7e1
agam <agamgupta1988@gmail.com>
2019-02-06 00:05:28 -0500

    Storage service folder created

--- src/main/java/com/pichincha/service/StorageService.java
+++ src/main/java/com/pichincha/service/StorageService.java
@@ -3,15 +3,19 @@ package com.pichincha.service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.chemistry.opencmis.client.api.CmisObject;
 import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.OperationContext;
 import org.apache.chemistry.opencmis.client.api.Repository;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.client.api.SessionFactory;
 import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
+import org.apache.chemistry.opencmis.commons.PropertyIds;
 import org.apache.chemistry.opencmis.commons.SessionParameter;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
+import org.apache.chemistry.opencmis.commons.enums.IncludeRelationships;
 
 public class StorageService {
 public static void main(String[] args) {
@@ -31,9 +35,21 @@ public static void main(String[] args) {
 	// create session
 	List<Repository> repositories = factory.getRepositories(parameters);
 	Session session = repositories.get(0).createSession();
-	Folder folder  = session.getRootFolder();
-	for (CmisObject child: folder.getChildren()) {
-	    System.out.println(child.getName());
+	String path = "/Documents/cmisrepo";
+	CmisObject cmisObject = session.getObjectByPath(path);
+	// prepare properties
+	Folder rootFolder  = (Folder) cmisObject;
+	String folderName  = "agam.gupta@tcs.com";
+	if (null==session.getObjectByPath(path+"/"+folderName)) {
+		Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put(PropertyIds.NAME, folderName);
+		properties.put(PropertyIds.OBJECT_TYPE_ID, "cmis:folder");
+		// create the folder
+		Folder newFolder = rootFolder.createFolder(properties);
+	} else {
+		System.out.println("folder exist");
 	}
+	
+	
 }
 }
